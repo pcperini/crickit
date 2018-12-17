903a3e79e1d1a2b335792501ae2837456ef17639
Patrick Perini <pcperini@gmail.com>
2018-12-17 11:01:29 -0800

    v1.0.1

--- src/crickit.ts
+++ src/crickit.ts
@@ -17,7 +17,7 @@ function validate<T>(value: T,
 }
 
 const main = async () => {
-  app.version('0.0.1')
+  app.version('1.0.1')
     .option('-a, --audioSource <path>', 'Path to audio')
     .option('-s, --audioStart <hh:mm:ss.ss>',
       'Timestamp to start audio',
